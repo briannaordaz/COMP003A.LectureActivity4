79ae0bec75a78c3895cfaf5f2d92ba8c4abb94cd
briannaordaz <briannaordaz5@gmail.com>
2026-02-04 23:55:08 -0800

    completed section A

--- COMP003A.LectureActivity.4/Program.cs
+++ COMP003A.LectureActivity.4/Program.cs
@@ -5,7 +5,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World!");
+        //Section A:
+        
+        //While Loop: a while loop executes the code if the condition is true 
+        int taskNumber = 1;
+        while (taskNumber <= 5)
+        {
+            Console.WriteLine($"Task {taskNumber} completed");
+            taskNumber++;
+        }
+        
+          
+        
+        
+        
+            
+            
     }
     
 }
